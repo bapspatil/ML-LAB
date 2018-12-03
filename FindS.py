02a57d958ac7a56737cdfaffac7f85682421dad8
Bapusaheb Patil <bapoo1998@gmail.com>
2018-12-03 19:26:01 +0530

    [EDIT] Replaced with 'o'

--- FindS.py
+++ FindS.py
@@ -2,11 +2,11 @@ import csv
 with open('trainingexamples.csv') as csvFile:
     data = [line[:-1] for line in csv.reader(csvFile) if line[-1] == "Y"]
 print("POSITIVE EXAMPLES ARE:{}".format(data))
-S = ['ɸ']*len(data[0])   # Initializing.
+S = ['o']*len(data[0])   # Initializing.
 print("output in each steps are:\n{}".format(S))
 for example in data:
     i = 0
     for feature in example:
-        S[i] = feature if S[i] == 'ɸ' or S[i] == feature else '?'
+        S[i] = feature if S[i] == 'o' or S[i] == feature else '?'
         i += 1
     print(S)
