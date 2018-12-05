3e9a73fe92103cc96eeedf2be3fbebda830f85e6
Bapusaheb Patil <bapoo1998@gmail.com>
2018-12-06 00:12:37 +0530

    [EDIT] Program 7 refactoring...

--- 7-bbn.py
+++ 7-bbn.py
@@ -1,32 +1,33 @@
 import numpy as np
 from urllib.request import urlopen
-import urllib
 import matplotlib.pyplot as plt # Visuals
-import seaborn as sns 
-import sklearn as skl
 import pandas as pd
-import pgmpy
+
 #import wrapt
-Cleveland_data_URL = 'http://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.hungarian.data'
 np.set_printoptions(threshold=np.nan) #see a whole array when we output it
+
+Cleveland_data_URL = 'http://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.hungarian.data'
 names = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal', 'heartdisease']
 heartDisease = pd.read_csv(urlopen(Cleveland_data_URL), names = names) #gets Cleveland data
 heartDisease.head()
 print(heartDisease.head())
+
 del heartDisease['ca']
 del heartDisease['slope']
 del heartDisease['thal']
 del heartDisease['oldpeak']
+
 heartDisease = heartDisease.replace('?', np.nan)
 heartDisease.dtypes
 print(heartDisease.dtypes)
 heartDisease.columns
+
 from pgmpy.models import BayesianModel
 from pgmpy.estimators import MaximumLikelihoodEstimator, BayesianEstimator
 model = BayesianModel([('age', 'trestbps'), ('age', 'fbs'), ('sex', 'trestbps'), ('sex', 'trestbps'), 
                        ('exang', 'trestbps'),('trestbps','heartdisease'),('fbs','heartdisease'),
                       ('heartdisease','restecg'),('heartdisease','thalach'),('heartdisease','chol')])
-                      
+
 # Learing CPDs using Maximum Likelihood Estimators
 model.fit(heartDisease, estimator=MaximumLikelihoodEstimator)
 
